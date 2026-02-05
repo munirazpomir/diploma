a1ffde3f9c711c40769a08bbf314da8ac8de8229
Munir <bred04li@gmail.com>
2026-02-05 20:03:52 +0300

    Добавление информации в QR

--- js/payment.js
+++ js/payment.js
@@ -52,9 +52,25 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('takenSeats', JSON.stringify(takenSeats));
 
     // QR
+    const seatsText = booking.seats
+  .map(s => `Ряд ${s.row}, Место ${s.seat}`)
+  .join('; ');
+
+const ticketText = `
+Билет № ${bookingCode}
+Фильм: ${booking.movie}
+Дата: ${booking.date}
+Время: ${booking.time}
+Зал: ${booking.hall}
+Места: ${seatsText}
+Стоимость: ${booking.price} руб.
+
+Билет действителен строго на свой сеанс
+`;
+
     qrContainer.innerHTML = '';
     new QRCode(qrContainer, {
-      text: bookingCode,
+      text: ticketText,
       width: 200,
       height: 200,
       correctLevel: QRCode.CorrectLevel.L
